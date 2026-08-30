ab19f7b85b4b93fc8f052559f1da4e34c48bdb7a
mrpachol <tomek.pacholski11@gmail.com>
2026-08-30 20:43:14 +0200

    Ignore generated files and configure Vercel

--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.B42CosGb.js","_app/immutable/chunks/Ba8sbP-i.js","_app/immutable/chunks/DpoBZXP3.js","_app/immutable/chunks/CkpaVJWi.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.CGVz_pUD.js","_app/immutable/chunks/Ba8sbP-i.js","_app/immutable/chunks/DpoBZXP3.js","_app/immutable/chunks/CkpaVJWi.js","_app/immutable/chunks/CKURdWym.js","_app/immutable/chunks/7LGr4wv0.js","_app/immutable/chunks/CFWeFpuw.js"];
-export const stylesheets = ["_app/immutable/assets/2.DlkrGSzg.css"];
-export const fonts = [];
